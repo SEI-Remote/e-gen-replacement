166e1d49e6e1eb55810791ef15c3e55dd452dba8
DavidStinson <49245298+DavidStinson@users.noreply.github.com>
2021-06-28 10:59:19 -0500

    rebuild express-generator

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+import { Router } from "express"
+const router = Router()
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+export { 
+  router
+}
---
+++ routes/users.js
@@ -0,0 +1,11 @@
+import { Router } from "express"
+const router = Router()
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+export {
+  router
+}
